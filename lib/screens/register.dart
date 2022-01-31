bf0c8383246a5f97b34abaefaa35318c90b18c5f
anvay2011041 <92028730+anvay2011041@users.noreply.github.com>
2022-01-31 12:20:14 +0530

    Update register.dart

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -1,9 +1,9 @@
 import 'package:authentication_app/screens/mobile.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:authentication_app/model/user_model.dart';
+import 'package:firebase_auth/firebase_auth.dart'; //authentication 
+import 'package:cloud_firestore/cloud_firestore.dart'; //cloud_firestore
+import 'package:authentication_app/model/user_model.dart'; 
 
 class Register extends StatefulWidget {
   const Register({Key? key}) : super(key: key);
