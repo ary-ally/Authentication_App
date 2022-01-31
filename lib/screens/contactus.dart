61807b714de7e4bd72deb18e6fbaf5ba19a919f5
anvay2011041 <92028730+anvay2011041@users.noreply.github.com>
2022-01-31 15:40:47 +0530

    changes

--- lib/screens/contactus.dart
+++ lib/screens/contactus.dart
@@ -19,98 +19,151 @@ class Contactus extends StatelessWidget {
           ),
         ),
         child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              SizedBox(
-                height: 150,
-                child: Image.asset("images/logo1.png", fit: BoxFit.contain),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                    horizontal: 20.0, vertical: 40.0),
-                child: Text(
-                  "Contributors :",
-                  style: TextStyle(
-                      fontSize: 22,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.blue.shade800),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                SizedBox(
+                  height: 150,
+                  child: Image.asset("images/logo1.png", fit: BoxFit.contain),
                 ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Card(
-                color: Colors.purpleAccent.shade100,
-                child: Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Icon(
-                        Icons.email_rounded,
-                        color: Colors.red.shade400,
-                      ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Text("Aryan Sharma",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.w500,
-                              fontSize: 20)),
-                    ],
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 20.0, vertical: 20.0),
+                  child: Center(
+                    child: Text(
+                      "Contributors",
+                      style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white70),
+                    ),
                   ),
                 ),
-              ),
-              Card(
-                color: Colors.purpleAccent.shade100,
-                child: Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Icon(
-                        Icons.email_rounded,
-                        color: Colors.red.shade400,
-                      ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Text("Abhinav Gautam",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.w500,
-                              fontSize: 20)),
-                    ],
+                SizedBox(
+                  height: 2,
+                ),
+                Card(
+                  color: Colors.white,
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Text("Aryan Sharma",
+                                style: TextStyle(
+                                    color: Colors.black38,
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 22)),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 10.0,
+                        ),
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.email_rounded,
+                              color: Colors.red.shade400,
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Text("Aryansharma4003@gmail.com",
+                                style: TextStyle(
+                                    color: Colors.purpleAccent,
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 20)),
+                          ],
+                        )
+                      ],
+                    ),
                   ),
                 ),
-              ),
-              Card(
-                color: Colors.purpleAccent.shade100,
-                child: Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Icon(
-                        Icons.email_rounded,
-                        color: Colors.red.shade400,
-                      ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Text("Anvay Raj",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.w500,
-                              fontSize: 20)),
-                    ],
+                Card(
+                  color: Colors.white,
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Text("Anvay Raj",
+                                style: TextStyle(
+                                    color: Colors.black38,
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 22)),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 10.0,
+                        ),
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.email_rounded,
+                              color: Colors.red.shade400,
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Text("Anvay2011041@akgec.ac.in",
+                                style: TextStyle(
+                                    color: Colors.purpleAccent,
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 20)),
+                          ],
+                        )
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            ],
+                Card(
+                  color: Colors.white,
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Text("Abhinav Gautam",
+                                style: TextStyle(
+                                    color: Colors.black38,
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 22)),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 10.0,
+                        ),
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.email_rounded,
+                              color: Colors.red.shade400,
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Text("Abhinav2015345@akgec.ac.in",
+                                style: TextStyle(
+                                    color: Colors.purpleAccent,
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 20)),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
