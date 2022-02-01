c335fe67880bca709964c31b40648cd74f40baa8
anvay2011041 <92028730+anvay2011041@users.noreply.github.com>
2022-02-01 17:23:40 +0530

    Update emailedit.dart

--- lib/screens/emailedit.dart
+++ lib/screens/emailedit.dart
@@ -3,7 +3,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-
 class Emailedit extends StatefulWidget {
   const Emailedit({Key? key}) : super(key: key);
 
@@ -11,9 +10,7 @@ class Emailedit extends StatefulWidget {
   _EmaileditState createState() => _EmaileditState();
 }
 
-
 class _EmaileditState extends State<Emailedit> {
-
   User? user = FirebaseAuth.instance.currentUser;
   UserModel loggedInUser = UserModel();
 
@@ -35,47 +32,38 @@ class _EmaileditState extends State<Emailedit> {
   final TextEditingController passwordController = TextEditingController();
   final _auth = FirebaseAuth.instance;
 
-
-
-
   @override
   Widget build(BuildContext context) {
-    final emailField=TextFormField(
+    final emailField = TextFormField(
       autofocus: false,
       controller: emailController,
       keyboardType: TextInputType.emailAddress,
-      validator: (value){
-        if(value!.isEmpty){
+      validator: (value) {
+        if (value!.isEmpty) {
           return ("Please Enter Your Email");
         }
-        if (!RegExp("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-z]")
-            .hasMatch(value)) {
+        if (!RegExp("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-z]").hasMatch(value)) {
           return ("Please Enter a valid email");
         }
         return null;
       },
-      onSaved: (value)
-      {
-        emailController.text= value!;
+      onSaved: (value) {
+        emailController.text = value!;
       },
       decoration: InputDecoration(
           prefixIcon: const Icon(Icons.mail),
           contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
           hintText: "Email",
-          border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(15)
-          )
-      ),
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(15))),
+      textInputAction: TextInputAction.next,
+    );
 
-      textInputAction: TextInputAction.next,);
-
-    final passwordField=TextFormField(
-
+    final passwordField = TextFormField(
       autofocus: false,
       controller: passwordController,
       obscureText: true,
       validator: (value) {
-        RegExp regex =  RegExp(r'^.{6,}$');
+        RegExp regex = RegExp(r'^.{6,}$');
         if (value!.isEmpty) {
           return ("Password is required for login");
         }
@@ -83,35 +71,40 @@ class _EmaileditState extends State<Emailedit> {
           return ("Enter Valid Password(Min. 6 Character)");
         }
       },
-      onSaved: (value)
-      {
-        passwordController.text= value!;
+      onSaved: (value) {
+        passwordController.text = value!;
       },
       decoration: InputDecoration(
           prefixIcon: const Icon(Icons.lock),
           contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
           hintText: "Password",
-          border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(15)
-          )
-      ),
-      textInputAction: TextInputAction.done,);
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(15))),
+      textInputAction: TextInputAction.done,
+    );
 
-    final verifyButton=Material(
+    final verifyButton = Material(
       elevation: 5,
       borderRadius: BorderRadius.circular(30),
       color: const Color(0xFF6B75CE),
       child: MaterialButton(
-        onPressed: (){
-
-        },
-        child: const Text("Reset", style: TextStyle(color: Colors.white,fontSize: 20,fontWeight: FontWeight.w300),),
+        onPressed: () {},
+        child: const Text(
+          "Reset",
+          style: TextStyle(
+              color: Colors.white, fontSize: 20, fontWeight: FontWeight.w300),
+        ),
         padding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
         minWidth: MediaQuery.of(context).size.width,
-      ),);
+      ),
+    );
 
     return Scaffold(
       resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        title: const Text('About'),
+        centerTitle: true,
+        backgroundColor: Colors.purpleAccent,
+      ),
       body: Container(
         decoration: const BoxDecoration(
           image: DecorationImage(
@@ -123,23 +116,45 @@ class _EmaileditState extends State<Emailedit> {
           child: Padding(
             padding: const EdgeInsets.all(35.0),
             child: Form(
-              key:_formkey ,
+              key: _formkey,
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  const SizedBox(height:150,
-                    child: Center(child: Text("Set New Password",style: TextStyle(color: Colors.white,fontSize: 50,fontWeight: FontWeight.w300,),)),
+                  const SizedBox(
+                    height: 150,
+                    child: Center(
+                        child: Text(
+                      "Set New Password",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 50,
+                        fontWeight: FontWeight.w300,
+                      ),
+                    )),
                   ),
-                  const SizedBox(height: 65,),
-                  emailField,
-                  const SizedBox(height: 65,),
-                  passwordField,
-                  const SizedBox(height: 35,),
-                  verifyButton,
-
-
-
+                  Card(
+                    child: Center(
+                      child: Padding(
+                        padding: const EdgeInsets.all(15.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            emailField,
+                            SizedBox(
+                              height: 10,
+                            ),
+                            passwordField,
+                            SizedBox(
+                              height: 30,
+                            ),
+                            verifyButton,
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
                 ],
               ),
             ),
